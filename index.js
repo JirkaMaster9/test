be9cd743b8c839fef4d170919318c76b88490ee1
Matysekxx <chaloupkamatej1353@gmail.com>
2026-03-17 07:33:40 +0100

    Komit by Uhlik

---
+++ index.js
@@ -0,0 +1,5 @@
+
+const image = document.getElementById("image");
+image.addEventListener("click", function () {
+    document.getElementById("secret-text").innerText = "Toto je náš třídní cikán"
+});
